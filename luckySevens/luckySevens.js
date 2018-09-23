bd44ef41dfc6990c08f7a7f39da485e9429e342a
Letrell15 <alandre_35@yahoo.com>
2018-09-23 11:30:41 -0400

    Update luckySevens.js
    Added comments

--- luckySevens/luckySevens.js
+++ luckySevens/luckySevens.js
@@ -1,3 +1,4 @@
+// Create a function that simulates the rolling of two dice and gathering the sum
 function rollDice()
 {
 	var rollOne = Math.floor(Math.random() * 6) + 1;
@@ -6,15 +7,19 @@ function rollDice()
 	return rollSum;
 
 }
-
+// Create a function that actually plays the game
 function betGame()
 {	
 	var startingBet = document.forms["numberGame"]["startingBet"].value;
 	var newAmount = 0;
 	var rollCount = 0;
+	
+	// Create an array so I can gather the number of rolls once I get to the max value
 	var resultArray = new Array();
 	resultArray[0] = startingBet;
 	var highestAmount = 0;
+	
+	// Displays an error if the bet value hasn't been entered or it equals zero
 	if (startingBet === "" || startingBet == 0) 
 	{
 		alert("Starting bet field invalid.");
@@ -22,6 +27,7 @@ function betGame()
 		return false;
 	}
 
+	// EVery game will have at least one roll so I set code up for the inital roll
 	console.log(rollDice());
 	rollCount = 1;
     	newAmount = startingBet;
@@ -36,6 +42,7 @@ function betGame()
 		resultArray.push(newAmount);
 	}
 	
+	// Execute a loop of rolls as long as the bet value is greater than 0
 	while (newAmount > 0)
 	{
 		console.log(rollDice());		
@@ -51,7 +58,11 @@ function betGame()
 			}
 		rollCount++;
 	}
+	
+	// Gets the max value from the array of results
 	highestAmount = Math.max.apply(null, resultArray);
+	
+	// Locates the position of the max value in the array to find out the number of rolls up to that point
 	var found = resultArray.indexOf(highestAmount);
 	
 	document.getElementById("results").style.display = "block";
@@ -59,6 +70,8 @@ function betGame()
 	document.getElementById("startingBet1").innerText = startingBet;
 	document.getElementById("rollCount").innerText = rollCount;
 	document.getElementById("highestAmount").innerText = highestAmount;
+	
+	// Adds one to the max value position to find the number of rolls up to that point
 	document.getElementById("rollCountAfterHigh").innerText = found + 1;
 	
 	return false;
